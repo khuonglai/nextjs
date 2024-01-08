aa03754030783f741682df753a6beda9af49bb2a
Khuong Lai <khuong.the.lai@gmail.com>
2024-01-08 20:34:19 +0000

    Fixed build error

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+import { ReactElement, JSXElementConstructor, ReactNode, PromiseLikeOfReactNode } from 'react';
 
 // Root page contains nav page and working page
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
@@ -14,7 +15,7 @@ function renderNavPage() {
     return <div className="w-full flex-none md:w-64"><SideNav /></div>
 }
 
-function renderPage(children) {
+function renderPage(children: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | PromiseLikeOfReactNode | null | undefined) {
     return <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>;
 }
 
